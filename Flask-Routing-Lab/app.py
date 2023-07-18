4c5398474d02b92b8e891b452c2f4de69df1ab7c
leshemha24-meet <leshem.hadad@gmail.com>
2023-07-18 16:26:56 +0300

    updated

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -1,18 +1,31 @@
 from flask import Flask, redirect, request, render_template, url_for
 
 
-app = Flask(  # Create a flask app
-    __name__,
-    template_folder='templates',  # Name of html file folder
-    static_folder='static'  # Name of directory for static files
-)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
-# Your code should be below
+@app.route('/')
+def main_page():
+    return render_template('home.html')
 
+@app.route('/product')
+def pro():
+    return render_template('product.html')
 
+@app.route('/cookies')
+def cook():
+    return render_template('cookie.html')
 
+@app.route('/cakes')
+def cake():
+    return render_template('cake.html')
 
-# Your code should be above
+@app.route('/cupcakes')
+def cup():
+    return render_template('cupcake.html')
+
+@app.route('/pies')
+def pie():
+    return render_template('pie.html')
 
 if __name__ == "__main__":  # Makes sure this is the main process
     app.run(debug=True)
--- Flask-Routing-Lab/static/js/script.js
+++ Flask-Routing-Lab/static/js/script.js
@@ -1,1 +1,15 @@
+// function cookie_fun() {
+// 	window.location.href = 'cookie.html';
+// }
 
+// function cake_fun() {
+// 	window.location.href = 'cake.html';
+// }
+
+// function pie_fun() {
+// 	window.location.href = 'pie.html';
+// }
+
+// function cupcake_fun() {
+// 	window.location.href = 'cupcake.html';
+// }
